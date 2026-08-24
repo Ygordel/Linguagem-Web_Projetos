3f3c365f4681816ba62117f176b189f8af8af966
Ygor Silva <124442841+Ygordel@users.noreply.github.com>
2026-08-24 01:02:18 -0300

    Add files via upload

---
+++ Aula_13/lgpd.js
@@ -0,0 +1,6 @@
+const btnLgpd = document.querySelector('#btn-lgpd');
+const conteudoLgpd = document.querySelector('#conteudo-lgpd');
+
+btnLgpd.addEventListener('click',function(){
+    conteudoLgpd.style.display = 'none';
+});
